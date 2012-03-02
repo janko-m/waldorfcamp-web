8b9c902a4998ba69017b42c9ab8b343a59e17eed
Matija Marohnić <matija.marohnic@gmail.com>
2012-03-02 15:51:20 +0100

    Separate stylesheets into partials

--- config/compass_config.rb
+++ config/compass_config.rb
@@ -1,6 +1,6 @@
 configure do
   Compass.configuration do |config|
-    config.sass_dir = 'views/css'
+    config.sass_dir = 'views/stylesheets'
     config.images_dir = 'public/images'
     config.http_images_path = File.join(config.http_path, 'images')
     config.line_comments = false
