1b2024acdaca6900e143cafd6ad1131834a3565c
Janko Marohnić <janko.marohnic@gmail.com>
2012-05-26 11:48:39 +0200

    The '/gallery' route now redirects to the default camp

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,9 @@ WaldorfCamp::Application.routes.draw do
   get "/workshops", :to => "pages#workshops"
   get "/workshops/:workshop", :to => "pages#workshop"
 
-  match "/gallery/(:camp)/expire_cache" => "pages#expire_camp_cache"
-  get "/gallery/(:camp)", :to => "pages#gallery", :as => :gallery
+  match "/gallery/:camp/expire_cache" => "pages#expire_camp_cache"
+  get "/gallery/:camp", :to => "pages#gallery", :as => :gallery
+  get "/gallery", :to => redirect("/gallery/2001-badija")
 
   match "/404", :to => "errors#not_found"
   match "/500", :to => "errors#internal_server_error"
