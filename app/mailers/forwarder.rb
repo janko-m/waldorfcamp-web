16ac09b9b8ca94e38299b9fe6422db1ca9d1bc37
Janko Marohnić <janko.marohnic@gmail.com>
2012-12-16 10:56:58 +0100

    Improve receiving emails

--- app/mailers/forwarder.rb
+++ app/mailers/forwarder.rb
@@ -1,28 +1,17 @@
-require "json"
-require "nokogiri"
+require_relative "forwarder/sendgrid_email_parser"
 
 class Forwarder < ActionMailer::Base
-  default from: "Outward Bound <#{ENV["SENDGRID_USERNAME"]}>", to: "janko.marohnic@gmail.com"
-
   def forward(params)
-    set_attachments(params)
-    fix_html(params)
+    @email = SendgridEmailParser.new(params).result
+    @email[:attachments].each { |filename, content| attachments[filename] = content }
 
-    @params = params
-    mail(subject: params[:subject]).deliver
-  end
+    p @email[:html]
+    p @email[:text]
 
-  def set_attachments(params)
-    JSON.parse(params[:"attachment-info"]).each do |name, info|
-      attachments[info["filename"]] = params[name].read
-    end
-  end
-
-  def fix_html(params)
-    document = Nokogiri::HTML(params[:html])
-    document.search(:img).each do |img|
-      img.remove if img[:src] =~ /^cid:[\w-]+$/
-    end
-    params[:html] = document.to_html
+    mail(
+      from: @email[:from],
+      to: "janko.marohnic@gmail.com",
+      subject: @email[:subject],
+    ).deliver
   end
 end
